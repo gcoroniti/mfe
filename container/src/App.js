305264381f3d0f5c9b3644e9191f0772b3e920c3
Gabriele Coroniti <gabriele.coroniti@gmail.com>
2021-10-27 09:20:16 +0200

    ndc

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
     return <div>
-        <h1>Ciao dal container!!!</h1>
+        <h1>Ciao dal container!</h1>
         <hr />
         <MarketingApp />
     </div>;
