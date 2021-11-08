8689c27e75418d6d32db24d7d8870f572ad79a89
Gabriele Coroniti <gabriele.coroniti@gmail.com>
2021-11-08 18:20:54 +0100

    signin sign out

--- container/src/components/AuthApp.js
+++ container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
     const ref = useRef(null);
     const history = useHistory();
 
@@ -16,6 +16,11 @@ export default () => {
                     history.push(nextPathname);
                 }
             },
+            onSignIn
+            // equivalente di
+            // onSignIn: () => {
+            //     onSignIn();
+            // }
         });
 
         history.listen(onParentNavigate);
